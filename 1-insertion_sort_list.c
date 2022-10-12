94a7b14b6103f4560eaf45bdc0a77ba86c986139
meron <meron@example.com>
2022-10-12 02:48:04 -0700

    task 1 second trial

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,22 +7,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *t;
-t = *list;
-while (p->next != NULL)
+listint_t *swap, *next;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+
+swap = (*list)->next;
+while (swap != NULL)
 {
-if (list->n > list->next->n)
+next = swap->next;
+while (swap->prev != NULL && swap->prev->n > swap->n)
 {
-switchs = list->n;
-list->n = list->next->n;
-list->next->n = switchs;
-if (list->previos->n > list->n)
-{
-switchs = list->previos->n;
-list->previos->n = list->n;
-list->n = switchs;
+swap->prev->next = swap->next;
+if (swap->next != NULL)
+swap->next->prev = swap->prev;
+swap->next = swap->prev;
+swap->prev = swap->next->prev;
+swap->next->prev = swap;
+if (swap->prev == NULL)
+*list = swap;
+else
+swap->prev->next = swap;
+print_list(*list);
 }
-print_list(list);
-}
+swap = next;
 }
 }
